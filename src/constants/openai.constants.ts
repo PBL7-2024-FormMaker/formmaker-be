a744ab2143056fcfdee2df98aae44d35f0b81ec6
Ho Thuy Tien <94684584+ThuyTien189@users.noreply.github.com>
2024-05-30 11:26:17 +0700

    fix: change the propmt to create form (#19)
    3a0ukVvE

--- src/constants/openai.constants.ts
+++ src/constants/openai.constants.ts
@@ -22,6 +22,7 @@ list of elements:
         Email: For inputting the email address
         config: {
             fieldLabel: element title (required),
+            required: indicates if the email is mandatory (required),
             sublabel: sub-label for email input field (optional, default value: 'Type your email address'),
         }
     },
@@ -29,6 +30,7 @@ list of elements:
         Fullname: For inputting the name
         config: {
             fieldLabel: question relates to name (required),
+            required: indicates if the full name is mandatory (required),
             sublabels: {
                 firstName: sublabel for the first name input (optional, default value: 'First Name'),
                 lastName: sublabel for the last name input (optional, default value: 'Last Name')
@@ -39,6 +41,7 @@ list of elements:
         Address: For inputting the address
         config: {
             fieldLabel: question relates to address (required),
+            required: indicates if the address is mandatory (required),
             sublabels: {
                 street: sublabel for the street input field (optional, default value: 'Street'),
                 ward: sublabel for the ward input field (optional, default value: 'Ward'),
@@ -51,6 +54,7 @@ list of elements:
         Phone: For inputting the phone number
         config: {
             fieldLabel: question relates to phone (required),
+            required: indicates if the phone is mandatory (required),
             sublabel: sublabel for phone number phone field (optional, default value: 'Type your phone number')
         }
     },
@@ -58,6 +62,7 @@ list of elements:
         Datepicker: For inputting the date
         config: {
             fieldLabel: question relates to date (required),
+            required: indicates if the date picker is mandatory (required),
             sublabel: sublabel for date input field (optional, default value: 'Type your date')
         }
     },
@@ -65,22 +70,25 @@ list of elements:
         Single Choice: For question has multiple options and note that has only one answer and input format contain that the answer only is one string or if has no answer use this element
         config: {
             fieldLabel: question of single choice (required),
+            required: indicates if the single choice is mandatory (required),
             options: the available options to answer the question (optional, default value: []),
-            otherOption: the option for user to choose out of the list (optional, default value: null)
+            otherOption: the option for user to choose out of the list (optional, default value: {isDisplayed: false, text: ""})
         }
     },
     {
         Multiple Choice: For question has multiple options and note that has multiple answers and input format contain that the answers are a array string
         config: {
             fieldLabel: question of multiple choice (required),
+            required: indicates if the multiple choice is mandatory (required),
             options: the available options to answer the question (optional, default value: []),
-            otherOption: the option for user to choose out of the list (optional, default value: null)
+            otherOption: the option for user to choose out of the list (optional, default value: {isDisplayed: false, text: ""})
         } 
     },
     {
         Time: For inputting the time
         config: {
             fieldLabel: question relates to only time (required),
+            required: indicates if the time is mandatory (required),
             sublabels: {
                 hour: sublabel for hour input field (optional, default value: 'hour'),
                 minutes: sublabel for minutes input field (optional, default value: 'minutes')
@@ -91,6 +99,7 @@ list of elements:
         Short Text: Element is for asking a question but not contains the specific questions above
         config: {
             fieldLabel: question of short text (required),
+            required: indicates if the short text is mandatory (required),
             sublabel: sublabel for short text field (optional, default value: 'Type your answer')
         }
     },
@@ -98,6 +107,7 @@ list of elements:
         Long Text: Element is like 'Short Text' element but if you think maybe have a lot of characters in the answer you use 'Long Text' instead. For instance, description question, the opinion question,...
         config: {
             fieldLabel: question of long text (required),
+            required: indicates if the long text is mandatory (required),
             sublabel: sublabel for long text field (optional, default value: 'Type your answer')
         }
     },
@@ -105,6 +115,7 @@ list of elements:
         Dropdown: Element is for asking a question which has multiple options and note that has only one answer and if more than 6 options,
         config: {
             fieldLabel: question of dropdown (required),
+            required: indicates if the dropdown is mandatory (required),
             sublabel: sublabel for dropdown field (optional, default value: 'Choose your answer'),
             options: the available options to answer the question (optional, default value: [])
         }
